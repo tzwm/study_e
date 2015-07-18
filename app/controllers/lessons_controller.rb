7c8fe6c6c8cf3990326416d647a3b4503fed1d2e
Samwise Wang <wangjunzhe4444@gmail.com>
2015-07-18 13:58:15 +0800

    Updated controllers

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,5 +3,14 @@ class LessonsController < ApplicationController
   end
 
   def create
+    lesson = Lesson.create! lesson_params
+
+    redirect_to new_lesson_questionnaire_path(lesson.id, phase: Questionnaire::PHASE[0])
+  end
+
+  private
+
+  def lesson_params
+    params.permit(:title, :description)
   end
 end
--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,9 +1,22 @@
 class QuestionnairesController < ApplicationController
+  before_action :find_and_auth_phase, :find_lesson
+
   def new
     
   end
 
   def create
     
+  end
+
+  private
+
+  def find_and_auth_phase
+    @phase = params[:phase]
+    redirect_to root_path unless @phase
+  end
+
+  def find_lesson
+    @lesson = Lesson.find(params[:lesson_id])
   end
 end
